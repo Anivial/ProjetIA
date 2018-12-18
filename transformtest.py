a45ec38259215406bbad1da5031f852d6ba0c84c
Anivial <alien360@live.fr>
2018-12-18 11:45:12 +0100

    Maj test

--- transformtest.py
+++ transformtest.py
@@ -1,10 +1,31 @@
 import io
+file = io.open("test", "w", encoding="utf-8")
 with io.open("data/word_phon.txt.1", 'r', encoding="utf-8") as source:
-
-    file = io.open("test", "w", encoding="utf-8")
-
     for line in source:
         line = line.strip().split("\t")
         file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
-
-    file.close()
+with io.open("data/word_phon.txt.2", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+with io.open("data/word_phon.txt.3", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+with io.open("data/word_phon.txt.4", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+with io.open("data/word_phon.txt.5", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+with io.open("data/word_phon.txt.6", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+with io.open("data/word_phon.txt.7", 'r', encoding="utf-8") as source:
+    for line in source:
+        line = line.strip().split("\t")
+        file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+file.close()
