ce7b2e1d6fae54f1b393ee16dd9d76ecbb1244c7
Anivial <alien360@live.fr>
2019-01-09 10:43:38 +0100

    Added gitignore

--- transformtest.py
+++ transformtest.py
@@ -4,6 +4,7 @@ with io.open("data/word_phon.txt.1", 'r', encoding="utf-8") as source:
     for line in source:
         line = line.strip().split("\t")
         file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+"""
 with io.open("data/word_phon.txt.2", 'r', encoding="utf-8") as source:
     for line in source:
         line = line.strip().split("\t")
@@ -28,4 +29,5 @@ with io.open("data/word_phon.txt.7", 'r', encoding="utf-8") as source:
     for line in source:
         line = line.strip().split("\t")
         file.write(" ".join(line[0]) + "\t" + " ".join(line[1:]) + "\n")
+"""
 file.close()
